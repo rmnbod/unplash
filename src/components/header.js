71efd69c8fc8bd9206078ae636f8b0fab0624d9d
Roman Bodnarchuk <roman.bodnarchuk@globallogic.com>
2020-02-17 12:54:33 +0200

    header cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,37 @@
 import React from "react";
-import home from '../assets/icons/home.svg';
-import collection from '../assets/icons/collection.svg';
-import upload from '../assets/icons/upload.svg';
-import search from '../assets/icons/search.svg';
-import '../App.css';
+import home from "../assets/icons/home.svg";
+import collection from "../assets/icons/collection.svg";
+import upload from "../assets/icons/upload.svg";
+import search from "../assets/icons/search.svg";
+import "../App.css";
 
 const header = () => {
-    return (
-        <header>
-            <nav>
-                <ul className='nav-list'>
-                    <li>
-                        <a className='header-nav-item' href='#home'>
-                            <img src={home} alt='Home' />
-                        </a>
-                    </li>
-                    <li>
-                        <a className='header-nav-item' href='#collections'>
-                            <img src={collection} alt='Home' />
-                        </a>
-                    </li>
-                    <li>
-                        <a className='header-nav-item' href='#upload'>
-                            <img src={upload} alt='Home' />
-                        </a>
-                    </li>
-                    <li>
-                        <a className='header-nav-item' href='#search'>
-                            <img src={search} alt='Home' />
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header>
+      <ul id="nav-list">
+        <li>
+          <a className="header-nav-item" href="#home">
+            <img src={home} alt="Home" />
+          </a>
+        </li>
+        <li>
+          <a className="header-nav-item" href="#collections">
+            <img src={collection} alt="Home" />
+          </a>
+        </li>
+        <li>
+          <a className="header-nav-item" href="#upload">
+            <img src={upload} alt="Home" />
+          </a>
+        </li>
+        <li>
+          <a className="header-nav-item" href="#search">
+            <img src={search} alt="Home" />
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
 export default header;
